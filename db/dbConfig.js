843a33e6ff5e77f0024c3105222815f41e992784
neway-k <newksm21@gmail.com>
2025-10-30 00:09:57 +0300

    db updated

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const mysql = require("mysql2");
+const fs = require("fs");
 
 const dbConnection = mysql.createPool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync(__dirname + "/ca.pem"),
+  },
   connectionLimit: 10,
 });
 
